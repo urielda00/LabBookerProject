0deb2c31e28f83b812d5c494cd9208193ee09903
MoeYousef1 <mohammadyousef08@gmail.com>
2025-03-20 22:39:02 +0200

    added 0.0.0.0 to app.listen in server.js

--- backend/server.js
+++ backend/server.js
@@ -290,7 +290,7 @@ process.on("SIGINT", async () => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log("[SERVER] Startup Complete");
 });
