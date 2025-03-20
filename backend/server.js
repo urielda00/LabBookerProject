45b2aa0184c46a44cc621a26cbb3a58b86fac7f4
MoeYousef1 <mohammadyousef08@gmail.com>
2025-03-20 19:46:24 +0200

    updated cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://labbooker-frontend.onrender.com"],
 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
